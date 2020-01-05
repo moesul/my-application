ed18c81129307378ffa3a71112b5e1955d3ef900
Moe Suleiman <msuleiman@frostkeep.com>
2020-01-04 17:10:30 -0800

    This is a test

--- app/src/main/java/com/moesul/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/moesul/msgshareapp/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.moesul.msgshareapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
     class MainActivity : AppCompatActivity() {
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-            btnShowToast.setOnClickListener{
-                Log.i("MainActivity", "Button was clicked")
-            }
+        btnShowToast.setOnClickListener{
+            Log.i("MainActivity", "Button was clicked")
+            Toast.makeText(this, "Button was clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSend.setOnClickListener {
+            val message: String = txtMsg.text.toString();
+            Toast.makeText(this, "\"$message\" message will be sent", Toast.LENGTH_SHORT).show()
+
+            // use intent to navigate to SecondActivity class (using java class reflection)
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
     }
+}
